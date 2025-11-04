3bb5136aecbf4356874a11aee1bf59008a75c520
Kschoenaker <0930147@hr.nl>
2025-11-04 19:39:24 +0100

    menu kiezen moet nog ingollen voor admin pushen

---
+++ Project/Logic/AdminLogic.cs
@@ -0,0 +1,20 @@
+public static class AdminLogic
+{
+    // als een admin heb ik een ader statrtchemm
+
+    // the going to menu's logic 
+    public static void NewDish(string dishName, double dishPrice, string dishType)
+    {
+
+    }
+
+    public static void NewDish()
+    {
+
+    }
+    public static void AddnewTheme()
+    {
+
+
+    }
+}
