3d3ef7ed82a7bb98ab9d5a3307c52ff964b1fa73
NoahE2005 <engeldal.noah@gmail.com>
2025-11-12 14:59:27 +0100

    Dish model update

--- Project/DataModels/DishModel.cs
+++ Project/DataModels/DishModel.cs
@@ -1,15 +1,13 @@
 public class DishModel
 {
     public int ID { get; set; }
-    public int Theme_ID { get; set; }
     public string DishName { get; set; }
     public double DishPrice { get; set; }
     public string DishType { get; set; }
 
-    public DishModel(int id, int theme_id, string dishName, double dishPrice, string dishType)
+    public DishModel(int id, string dishName, double dishPrice, string dishType)
     {
         ID = id;
-        Theme_ID = theme_id;
         DishName = dishName;
         DishPrice = dishPrice;
         DishType = dishType;
